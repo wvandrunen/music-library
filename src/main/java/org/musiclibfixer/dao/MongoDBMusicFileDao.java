6674f024f39cbb4475ecdf7aecacff891ec43db5
Wouter van Drunen <wvandrunen@gmail.com>
2014-03-01 19:14:17 +0100

    Refactoring

---
+++ src/main/java/org/musiclibfixer/dao/MongoDBMusicFileDao.java
@@ -0,0 +1,26 @@
+package org.musiclibfixer.dao;
+
+import org.musiclibfixer.config.MongoCollectionNames;
+import org.musiclibfixer.model.MusicFile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MongoDBMusicFileDao implements MusicFileDao {
+
+    private MongoOperations mongoOperations;
+    private String targetCollection;
+
+    @Autowired
+    public MongoDBMusicFileDao(MongoOperations mongoOperations, String targetCollection) {
+        this.mongoOperations = mongoOperations;
+        this.targetCollection = targetCollection;
+    }
+
+    @Override
+    public MusicFile insert(MusicFile musicFile) {
+        mongoOperations.insert(musicFile, targetCollection);
+        return musicFile;
+    }
+}
---
+++ src/test/java/org/musiclibfixer/dao/MongoDBMusicFileDaoIntegrationTest.java
@@ -0,0 +1,46 @@
+package org.musiclibfixer.dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.musiclibfixer.config.MongoCollectionNames;
+import org.musiclibfixer.config.SpringMongoConfig;
+import org.musiclibfixer.model.MusicFile;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.data.mongodb.core.MongoOperations;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * This integration test uses a MongoDB instance running on localhost on the standard port
+ */
+public class MongoDBMusicFileDaoIntegrationTest {
+
+    private MongoOperations mongoOperations = null;
+    private String musicCollection;
+
+    @Before
+    public void setUp() {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(SpringMongoConfig.class);
+        mongoOperations = (MongoOperations) ctx.getBean("mongoTemplate");
+
+        musicCollection = MongoCollectionNames.MUSIC_COLLECTION_NAME_INTEGRATION_TEST;
+    }
+
+    @After
+    public void tearDown() {
+        mongoOperations.dropCollection(musicCollection);
+    }
+
+    @Test
+    public void whenMusicFileInsertedGivenMusicFileThenSavedToMongoDB() {
+        MusicFileDao musicFileDao = new MongoDBMusicFileDao(mongoOperations, musicCollection);
+
+        MusicFile musicFile = new MusicFile("Sick Again", "Led Zeppelin", "Physical Graffiti");
+
+        MusicFile musicFileWithObjectId = musicFileDao.insert(musicFile);
+
+        assertThat(musicFileWithObjectId.getId()).isNotNull();
+    }
+
+}
