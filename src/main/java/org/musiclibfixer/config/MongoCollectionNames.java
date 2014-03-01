a193458d23a181259734b0181674aa27407590dc
Wouter van Drunen <wvandrunen@gmail.com>
2014-03-01 18:25:55 +0100

    Logging & Refactoring
    - Updated MongoDB collection
    - Using logback
    - Renamed implementation class
    - Using Mongo Labs

--- src/main/java/org/musiclibfixer/config/MongoCollectionNames.java
+++ src/main/java/org/musiclibfixer/config/MongoCollectionNames.java
@@ -4,4 +4,6 @@ public class MongoCollectionNames {
 
     public static final String MUSIC_COLLECTION_NAME = "music";
 
+    public static final String MUSIC_COLLECTION_NAME_INTEGRATION_TEST = "music-int-test";
+
 }
--- src/main/java/org/musiclibfixer/config/SpringMongoConfig.java
+++ src/main/java/org/musiclibfixer/config/SpringMongoConfig.java
@@ -1,20 +1,25 @@
 package org.musiclibfixer.config;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
-
 import java.net.UnknownHostException;
+
+import static java.util.Arrays.asList;
 
 @Configuration
 public class SpringMongoConfig {
 
     public @Bean
     MongoDbFactory mongoDbFactory() throws UnknownHostException {
-        return new SimpleMongoDbFactory(new MongoClient(), "musicdb");
+        MongoCredential credential = MongoCredential.createMongoCRCredential("admin", "music-db", "admin".toCharArray());
+        MongoClient mongoClient = new MongoClient(new ServerAddress("ds057877.mongolab.com", 57877), asList(credential));
+        return new SimpleMongoDbFactory(mongoClient, "music-db");
     }
 
     public @Bean
