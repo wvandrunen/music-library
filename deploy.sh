89b21409d519d7756908566cb35aceacb8d63413
Wouter van Drunen <wvandrunen@gmail.com>
2014-03-22 14:14:28 +0100

    Added deploy script to Heroku

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,3 @@ echo "| Running Maven in Back-end project"
 echo "================================================================================"
 cd Back-end
 mvn install
-echo "================================================================================"
-echo "| Deploying Back-end project to Heroku"
-echo "================================================================================"
-cd ..
-git subtree push --prefix Back-end heroku master
